a38de18df80dc02b8990918212b6705c0519b1db
Ketchup918 <kchp918@gmail.com>
2025-12-22 15:10:47 +0900

    Level

--- Assets/SJH/Script/Player.cs
+++ Assets/SJH/Script/Player.cs
@@ -90,6 +90,7 @@ public class Player : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         if(collision.gameObject.CompareTag("Wall"))
         {
             ParticleSystem particle = Instantiate(particleP);
+            particle.gameObject.transform.localScale = new Vector2(0.4f, 0.4f);
             particle.gameObject.transform.position = collision.GetContact(0).point;
             float atan = Mathf.Atan2(collision.GetContact(0).normal.x, collision.GetContact(0).normal.y);
             float angle =  (collision.GetContact(0).normal.x != 0 ? -atan : atan) * Mathf.Rad2Deg;
