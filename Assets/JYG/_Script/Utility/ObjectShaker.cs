26883743b06e4bf226e62acfda3130ee83b26c2b
Nugusaeyo <chsj0702@gmail.com>
2025-12-22 19:32:04 +0900

    EnemyDead

---
+++ Assets/JYG/_Script/Utility/ObjectShaker.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ObjectShaker : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    [SerializeField] private float duration = 1f;
+
+    public void ShakeTarget()
+    {
+        target.transform.DOShakePosition(duration);
+    }
+}
