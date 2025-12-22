d12d69aec75da42bca55150c9159756c50e84a36
Ketchup918 <kchp918@gmail.com>
2025-12-22 20:08:50 +0900

    ToolTip

--- Assets/SJH/Script/GameManager.cs
+++ Assets/SJH/Script/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
   
     public Player Player;
     public TurnManager TurnManager;
-
+    public ToolTip Tooltip;
     private void Awake()
     {
         if(Instance == null)
@@ -25,5 +25,6 @@ public class GameManager : MonoBehaviour
         Player.particleP = bounceParticle;
         Player.particleP2 = bounceParticle2;
         TurnManager = Instantiate(TurnManager);
+        Tooltip = Instantiate(Tooltip);
     }
 }
