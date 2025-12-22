3e5ad8d70b76872b09534fe5f2d2f790f035a11d
Nugusaeyo <chsj0702@gmail.com>
2025-12-22 16:35:51 +0900

    Enemy

---
+++ Assets/JYG/_Script/SinEnemy.cs
@@ -0,0 +1,14 @@
+﻿using Assets.JYG._Script;
+using DG.Tweening;
+using UnityEngine;
+
+public class SinEnemy : Enemy
+{
+    protected override void EnemyMove()
+    {
+        _moveTween.Kill();
+        _moveTween = DOTween.Sequence();
+        Vector3 targetPos = (_target.position - transform.position).normalized;
+
+    }
+}
